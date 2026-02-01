61293ebf07660f643b0910ebf5ff0d9d85b0039d
nzzi <nzzi@woowahan.com>
2026-02-01 15:04:53 +0900

    Initial commit

---
+++ src/test/java/com/toy/cnr/CopsNrobbersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.toy.cnr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CopsNrobbersApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
