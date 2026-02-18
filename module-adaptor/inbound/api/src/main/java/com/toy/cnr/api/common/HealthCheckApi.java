3150884072758d7890fa4aaba8d54d8ed0ff490a
nzzi <cholnh486@gmail.com>
2026-02-19 03:00:33 +0900

    test: 헬스체크 구성

---
+++ module-adaptor/inbound/api/src/main/java/com/toy/cnr/api/common/HealthCheckApi.java
@@ -0,0 +1,19 @@
+package com.toy.cnr.api.common;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthCheckApi {
+
+    @GetMapping("/elb-health")
+    public String health() {
+        return "OK";
+    }
+
+    // for debug
+    @GetMapping("/version")
+    public String version() {
+        return "1.0.0";
+    }
+}
